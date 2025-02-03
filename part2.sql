127ba53d7ab5aa0f5a22ae062476c0416213b459
bcsteelman <115572862+bcsteelman@users.noreply.github.com>
2025-02-03 11:31:19 -0700

    Add files via upload

---
+++ part2.sql
@@ -0,0 +1,38 @@
+create database Library
+
+use Library
+
+create table Authors (
+AuthorID int primary key identity,
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+DOB varchar (25) not null
+);
+
+create table Books (
+BookID int primary key identity,
+Genre varchar (25) not null,
+AuthorID int not null,
+foreign key (AuthorID) references Authors(AuthorID)
+);
+
+create table Borrowers (
+BorrowerID int primary key identity,
+FirstName varchar(25) not null,
+LastName varchar(25) not null,
+DOB varchar (25)
+);
+
+create table BorrowedBooks (
+BookID int not null,
+BorrowerID int not null,
+DueDate varchar(25) not null,
+TransactionDate varchar(25) not null,
+primary key (BookID, BorrowerID),
+foreign key (BookID) references Books(BookID),
+foreign key (BorrowerID) references Borrowers(BorrowerID)
+);
+
+
+
+
